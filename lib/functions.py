78aeb175863fe78c4c85fa70643013d3d04ea5ef
Zacharia Ndinguri <zacthuku@gmail.com>
2025-05-07 06:43:36 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f'Hello, {name}!')
+   
     pass
 
 def greet_with_default(name="programmer"):
+    print(f'Hello, {name}!')
     pass
 
 def add(num1, num2):
+    return(num1 + num2)
     pass
 
 def halve(number):
+    return(number / 2)
     pass
